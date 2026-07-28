aaa1e7f6efbe5274c54dae80e45e888c026a0dfa
officialritu747-byte <officialritu747@gmail.com>
2026-07-28 20:09:07 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+let cart = [];
+
+function addToCart(name, price) {
+    cart.push({ name, price });
+    alert(name + " added to cart");
+}
+
+function buyNow(name, price) {
+    alert("Buying " + name + " - $" + price);
+}
+let cart = [];
+
+function addToCart(name, price) {
+    cart.push({ name, price });
+    alert(name + " added to cart");
+}
+
+function buyNow(name, price) {
+    alert("Buying " + name + " - $" + price);
+}
