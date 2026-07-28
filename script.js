2dd3fbc4d020c5f93d041fc479a6837404e05211
officialritu747-byte <officialritu747@gmail.com>
2026-07-28 20:14:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,14 @@ function addToCart(name, price) {
 function buyNow(name, price) {
     alert("Buying " + name + " - $" + price);
 }
+function addToCart(name, price) {
+    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    cart.push({
+        name: name,
+        price: price
+    });
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+    alert(name + " added to cart!");
+}
