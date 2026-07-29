8c6cf6c41566294279d56a41775e6c90818a4152
officialritu747-byte <officialritu747@gmail.com>
2026-07-29 11:08:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -268,3 +268,18 @@ loadProducts();
 }
 
 window.onload=loadProducts;
+// Product Search
+function searchProducts() {
+  let input = document.getElementById("searchInput").value.toLowerCase();
+  let products = document.getElementsByClassName("product");
+
+  for (let i = 0; i < products.length; i++) {
+    let name = products[i].getElementsByTagName("h3")[0].innerText.toLowerCase();
+
+    if (name.indexOf(input) > -1) {
+      products[i].style.display = "";
+    } else {
+      products[i].style.display = "none";
+    }
+  }
+}
