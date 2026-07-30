85cc852ce39051a016174a1d66015866a2320cb2
officialritu747-byte <officialritu747@gmail.com>
2026-07-30 08:41:13 +0530

    Update Products.js

--- Products.js
+++ Products.js
@@ -22,3 +22,41 @@ const ProductEngine = {
     this.products = this.products.filter(p => p.id !== id);
   }
 };
+class Product {
+    constructor(id, name, category, price, stock, image, sellerId, description) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.stock = stock;
+        this.image = image;
+        this.sellerId = sellerId;
+        this.description = description;
+        this.createdAt = new Date();
+    }
+}
+
+const testProduct = new Product(
+    "P001",
+    "Basmati Rice 5kg",
+    "Groceries",
+    599,
+    50,
+    "images/rice.jpg",
+    "SELLER001",
+    "Premium Quality Basmati Rice"
+);
+
+console.log(testProduct);
+const testProduct = new Product(
+    "P001",
+    "Basmati Rice 5kg",
+    "Groceries",
+    599,
+    50,
+    "images/rice.jpg",
+    "SELLER001",
+    "Premium Quality Basmati Rice"
+);
+
+console.log(testProduct);
