031979369fb8ab2dcfc0168c9121de69eefc36bd
officialritu747-byte <officialritu747@gmail.com>
2026-07-30 08:24:04 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,3 +2,28 @@ import "./products.js";
 
 import "./cart.js";
 console.log("Bazario Started");
+function signupUser() {
+
+    const name = document.getElementById("signupName").value;
+    const email = document.getElementById("signupEmail").value;
+    const password = document.getElementById("signupPassword").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+
+    if (name === "" || email === "" || password === "" || confirmPassword === "") {
+        alert("Please fill all fields");
+        return;
+    }
+
+    if (password !== confirmPassword) {
+        alert("Passwords do not match");
+        return;
+    }
+
+    alert("Signup successful (Frontend Test)");
+    console.log({
+        name,
+        email,
+        password
+    });
+
+}
