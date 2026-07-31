a7d1b841b4bb28051424ef86e9744adee30b3c0f
officialritu747-byte <officialritu747@gmail.com>
2026-07-31 19:50:39 +0530

    Create Checkout.js

---
+++ Checkout.js
@@ -0,0 +1,17 @@
+function placeOrder() {
+
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+    let address = document.getElementById("address").value;
+
+    if(name==="" || phone==="" || address===""){
+        alert("Please fill all details");
+        return;
+    }
+
+    localStorage.removeItem("cart");
+
+    alert("Order Placed Successfully!");
+
+    window.location.href = "order-success.html";
+}
