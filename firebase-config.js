278c05caafcfe948567d215be43e74472d2f8598
officialritu747-byte <officialritu747@gmail.com>
2026-07-31 11:38:22 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,27 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "YAHAN_APNI_API_KEY_PASTE_KARO",
+  apiKey: "YAHAN_APNI_API_KEY_PASTE_KARO",// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBaDLjF1nbsftZ6t03iFxGfSRsRfZHLqP0",
+  authDomain: "bazario-9bd2b.firebaseapp.com",
+  projectId: "bazario-9bd2b",
+  storageBucket: "bazario-9bd2b.firebasestorage.app",
+  messagingSenderId: "830078880682",
+  appId: "1:830078880682:web:2739e1d0251db9445ff9d3",
+  measurementId: "G-2HGW98SEN9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
   authDomain: "bazario-9bd2b.firebaseapp.com",
   projectId: "bazario-9bd2b",
   storageBucket: "bazario-9bd2b.firebasestorage.app",
