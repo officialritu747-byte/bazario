d2fc6895d209bdd54d6650f4ff4fde3d30d012c5
officialritu747-byte <officialritu747@gmail.com>
2026-08-01 08:42:56 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,29 +1,12 @@
+// Firebase v10 Configuration
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "YAHAN_APNI_API_KEY_PASTE_KARO",// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBaDLjF1nbsftZ6t03iFxGfSRsRfZHLqP0",
-  authDomain: "bazario-9bd2b.firebaseapp.com",
-  projectId: "bazario-9bd2b",
-  storageBucket: "bazario-9bd2b.firebasestorage.app",
-  messagingSenderId: "830078880682",
-  appId: "1:830078880682:web:2739e1d0251db9445ff9d3",
-  measurementId: "G-2HGW98SEN9"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  apiKey: "YOUR_API_KEY",
   authDomain: "bazario-9bd2b.firebaseapp.com",
   projectId: "bazario-9bd2b",
   storageBucket: "bazario-9bd2b.firebasestorage.app",
@@ -35,24 +18,6 @@ const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-export async function testFirebase() {
-    console.log("Firebase Connected Successfully");
-    return true;
-}
+export const storage = getStorage(app);
 
-testFirebase();
-db.collection("orders")
-  .add(order)
-  .then(() => {
-      localStorage.removeItem("cart");
-      window.location.href = "success.html";
-  })
-  .catch((error) => {
-      alert("Order Save Failed");
-      console.log(error);
-  });
-// Firestore
-const db = firebase.firestore();
-
-// Storage
-const storage = firebase.storage();
+console.log("Firebase Connected Successfully");
