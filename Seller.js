754a395df13af3b71410e6a2776ddf3869934934
officialritu747-byte <officialritu747@gmail.com>
2026-08-01 08:48:17 +0530

    Update Seller.js

--- Seller.js
+++ Seller.js
@@ -15,5 +15,53 @@ window.deleteProduct = async function(id){
         alert("Product Deleted");
 
     }
+import { db } from "./firebase-config.js";
+import {
+  collection,
+  addDoc,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+const productForm = document.getElementById("productForm");
+const productList = document.getElementById("productList");
+
+productForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const price = Number(document.getElementById("price").value);
+  const stock = Number(document.getElementById("stock").value);
+  const description = document.getElementById("description").value;
+
+  await addDoc(collection(db, "products"), {
+    name,
+    price,
+    stock,
+    description
+  });
+
+  alert("Product Uploaded");
+  productForm.reset();
+  loadProducts();
+});
+
+async function loadProducts() {
+  productList.innerHTML = "";
+
+  const snapshot = await getDocs(collection(db, "products"));
+
+  snapshot.forEach((doc) => {
+    const p = doc.data();
+
+    productList.innerHTML += `
+      <div class="card">
+        <h3>${p.name}</h3>
+        <p>₹${p.price}</p>
+        <p>Stock: ${p.stock}</p>
+      </div>
+    `;
+  });
+}
+
+loadProducts();
 }
